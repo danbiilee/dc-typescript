5f78a918d9d454a65467886b4fcbce5a46cdb73b
danbiilee <danbi.db@gmail.com>
2021-03-12 12:54:09 +0900

    Solve game.ts

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,34 @@
 /**
  * Let's make a game 🕹
  */
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+type Position = {
+  x: number;
+  y: number;
+};
+
+const position: Position = { x: 0, y: 0 };
+
+function move(dir: Direction) {
+  switch (dir) {
+    case 'up':
+      position.y++;
+      break; // swicth문 빠져나감
+    case 'down':
+      position.y--;
+      break;
+    case 'left':
+      position.x--;
+      break;
+    case 'right':
+      position.x++;
+      break;
+    default:
+      throw new Error(`Unknown Direction ${dir}!`);
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
