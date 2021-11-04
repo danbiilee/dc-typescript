18fafbcc041c96749ba125c08e5aaa39ffbdb064
dblee <danbi.db@gmail.com>
2021-11-04 12:49:21 +0900

    Add dialog input

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,46 +1,91 @@
 import { InputDialog } from "./components/dialog/dialog.js";
 import { Component } from "./components/component.js";
 import { ImageComponent } from "./components/page/item/image.js";
-import { NoteComponent } from "./components/page/item/notes.js";
-import { TodoComponent } from "./components/page/item/todo.js";
-import { VideoComponent } from "./components/page/item/video.js";
 import { Composable, PageComponent, PageItemComponent } from "./components/page/page.js";
+import { MediaSectionInput } from "./components/dialog/input/media-input.js";
+import { VideoComponent } from "./components/page/item/video.js";
+import { NoteComponent } from "./components/page/item/notes.js";
+import { TextSectionInput } from "./components/dialog/input/text-input.js";
+import { TodoComponent } from "./components/page/item/todo.js";
 
 class App {
   // page가 PageComponent일지 아닐지 모름 -> 커플링을 추상화해줌
   private readonly page: Component & Composable;
 
-  constructor(appRoot: HTMLElement) {
+  constructor(appRoot: HTMLElement, dialogRoot: HTMLElement) {
     this.page = new PageComponent(PageItemComponent); // DI
     this.page.attachTo(appRoot);
-
-    const image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
-    // Page 내부적으로 PageItemComponent로 한단계 더 감싸서 추가
-    this.page.addChild(image);
-
-    const video = new VideoComponent("Video Title", "https://www.youtube.com/watch?v=U2ZF0Vmzk-Q&t=3s");
-    this.page.addChild(video);
-
-    const note = new NoteComponent("Note Title", "Note Body");
-    this.page.addChild(note);
-
-    const todo = new TodoComponent("Todo Title", "Todo Item");
-    this.page.addChild(todo);
 
     const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
     imageBtn.addEventListener("click", () => {
       const dialog = new InputDialog();
+      const mediaSection = new MediaSectionInput();
 
-      dialog.setOnCloseListener(() => dialog.removeFrom(document.body));
+      dialog.addChild(mediaSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => dialog.removeFrom(dialogRoot));
       dialog.setOnSubmitListener(() => {
-        // 섹션을 만들어서 페이지 추가해주기
-      });
+        const image = new ImageComponent(mediaSection.title, mediaSection.url);
+        this.page.addChild(image);
 
-      dialog.attachTo(document.body);
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const videoBtn = document.querySelector("#new-video")! as HTMLButtonElement;
+    videoBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      const mediaSection = new MediaSectionInput();
+
+      dialog.addChild(mediaSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => dialog.removeFrom(dialogRoot));
+      dialog.setOnSubmitListener(() => {
+        const video = new VideoComponent(mediaSection.title, mediaSection.url);
+        this.page.addChild(video);
+
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const noteBtn = document.querySelector("#new-note")! as HTMLButtonElement;
+    noteBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      const textSection = new TextSectionInput();
+
+      dialog.addChild(textSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => dialog.removeFrom(dialogRoot));
+      dialog.setOnSubmitListener(() => {
+        const notes = new NoteComponent(textSection.title, textSection.body);
+        this.page.addChild(notes);
+
+        dialog.removeFrom(dialogRoot);
+      });
+    });
+
+    const todoBtn = document.querySelector("#new-todo")! as HTMLButtonElement;
+    todoBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+      const textSection = new TextSectionInput();
+
+      dialog.addChild(textSection);
+      dialog.attachTo(dialogRoot);
+
+      dialog.setOnCloseListener(() => dialog.removeFrom(dialogRoot));
+      dialog.setOnSubmitListener(() => {
+        const notes = new TodoComponent(textSection.title, textSection.body);
+        this.page.addChild(notes);
+
+        dialog.removeFrom(dialogRoot);
+      });
     });
   }
 }
 
 // .document 요소가 동적으로 만들어지는 것이 아니고 이미 HTML 문서에 정의되어 있기 때문에
 // 확신있게 null이 아니라고 Type Aseertion 사용
-new App(document.querySelector(".document")! as HTMLElement);
+new App(document.querySelector(".document")! as HTMLElement, document.body);
