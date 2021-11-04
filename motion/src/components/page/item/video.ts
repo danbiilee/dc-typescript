6364aeb445a99830abe0e4d82f7d9281a4886402
dblee <danbi.db@gmail.com>
2021-11-04 12:30:55 +0900

    주석 제거

--- motion/src/components/page/item/video.ts
+++ motion/src/components/page/item/video.ts
@@ -15,15 +15,10 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
             <h3 class="video__title"></h3>
           </section>`);
 
-    const iframe = this.element.querySelector(
-      ".video__iframe"
-    )! as HTMLIFrameElement;
+    const iframe = this.element.querySelector(".video__iframe")! as HTMLIFrameElement;
     iframe.src = this.convertToEmbeddedUrl(url);
-    console.log(this.convertToEmbeddedUrl(url));
 
-    const titleElement = this.element.querySelector(
-      ".video__title"
-    )! as HTMLHeadingElement;
+    const titleElement = this.element.querySelector(".video__title")! as HTMLHeadingElement;
     titleElement.textContent = title;
   }
 
@@ -41,7 +36,6 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     const regExp =
       /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9(-|_)]{11}))|(?:youtu.be\/([a-zA-Z0-9(-|_)]{11})))/;
     const match = url.match(regExp);
-    console.log(match);
     const videoId = match ? match[1] || match[2] : undefined;
     if (videoId) {
       return `https://www.youtube.com/embed/${videoId}`;
@@ -49,13 +43,3 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
     return url;
   }
 }
-
-// <iframe
-//   width="1142"
-//   height="721"
-//   src="https://www.youtube.com/embed/U2ZF0Vmzk-Q"
-//   title="YouTube video player"
-//   frameborder="0"
-//   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-//   allowfullscreen
-// ></iframe>
